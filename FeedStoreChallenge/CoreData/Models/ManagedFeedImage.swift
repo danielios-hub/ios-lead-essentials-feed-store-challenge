1f335c935223232e96dc03daf062d603852872c1
Daniel <1asir.daniel.gallego@gmail.com>
2021-08-25 19:44:20 +0200

    In `ManagedFeedImage` change cache property to the same type defined in the data model

--- FeedStoreChallenge/CoreData/Models/ManagedFeedImage.swift
+++ FeedStoreChallenge/CoreData/Models/ManagedFeedImage.swift
@@ -14,7 +14,7 @@ final class ManagedFeedImage: NSManagedObject {
 	@NSManaged var imageDescription: String?
 	@NSManaged var location: String?
 	@NSManaged var url: URL
-	@NSManaged var cache: NSManagedObject
+	@NSManaged var cache: ManagedCache
 
 	static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
 		return NSOrderedSet(array: localFeed.map { local in
